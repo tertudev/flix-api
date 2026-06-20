67c6d799b92ccac1cebd6eb5200f23b7a305d2ef
tertudev <wendelgomesdh7@gmail.com>
2026-06-20 13:50:50 -0400

    feat(views): implement genre view for GET and POST requests

--- genres/views.py
+++ genres/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from genres.models import Genre
 
-# Create your views here.
+@csrf_exempt
+def genre_view(request):
+    if request.method == 'GET':
+        genres = Genre.objects.all()
+        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+        return JsonResponse(data, safe=False)
+
+    elif request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        new_genre = Genre(name=data['name'])
+        new_genre.save()
+        return JsonResponse(
+            {'id': new_genre.id, 'name': new_genre.name}, status=201,
+        )
