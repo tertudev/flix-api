deba3cdfb6e84b7540cbee629174e2b65add37dd
tertudev <wendelgomesdh7@gmail.com>
2026-06-20 12:24:41 -0400

    refactor(urls): remove unused hello_view and associated path

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import JsonResponse
-
-def hello_view(request):
-    return JsonResponse({'id': 1, 'name': 'Titanic', 'year': 1998})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_view),
 ]
