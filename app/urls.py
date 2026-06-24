802b4f39618659533d005c3553b90370eb963105
tertudev <wendelgomesdh7@gmail.com>
2026-06-24 11:12:18 -0400

    fix(urls): update genre view path to use genre_create_list_view

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_view
+from genres.views import genre_create_list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_view, name='genre'),
+    path('genres/', genre_create_list_view, name='genre-create-list'),
 ]
